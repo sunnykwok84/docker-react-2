7e0815b1eeb983217a8860aff4e0445353d6970e
sunnykwok <trillionsun84@gmail.com>
2023-01-08 22:33:57 +0800

    welcome message update

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-	  Hi There Bye There World BB
+	  I was changed!
         </p>
         <a
           className="App-link"
